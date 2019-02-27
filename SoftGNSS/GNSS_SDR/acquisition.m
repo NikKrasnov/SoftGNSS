7aedd24953740089221131efbe9f7fce483af368
Krasnov Nikita <krasnov_na@inbox.ru>
2019-02-27 14:47:55 +0300

    First commit - deleting comments

--- SoftGNSS/GNSS_SDR/acquisition.m
+++ SoftGNSS/GNSS_SDR/acquisition.m
@@ -18,32 +18,6 @@ function acqResults = acquisition(longSignal, settings)
 %                       field "carrFreq" is set to 0 if the signal is not
 %                       detected for the given PRN number. 
  
-%--------------------------------------------------------------------------
-%                           SoftGNSS v3.0
-% 
-% Copyright (C) Darius Plausinaitis and Dennis M. Akos
-% Written by Darius Plausinaitis and Dennis M. Akos
-% Based on Peter Rinder and Nicolaj Bertelsen
-%--------------------------------------------------------------------------
-%This program is free software; you can redistribute it and/or
-%modify it under the terms of the GNU General Public License
-%as published by the Free Software Foundation; either version 2
-%of the License, or (at your option) any later version.
-%
-%This program is distributed in the hope that it will be useful,
-%but WITHOUT ANY WARRANTY; without even the implied warranty of
-%MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%GNU General Public License for more details.
-%
-%You should have received a copy of the GNU General Public License
-%along with this program; if not, write to the Free Software
-%Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
-%USA.
-%--------------------------------------------------------------------------
-
-%CVS record:
-%$Id: acquisition.m,v 1.1.2.12 2006/08/14 12:08:03 dpl Exp $
-
 %% Initialization =========================================================
 
 % Find number of samples per spreading code
